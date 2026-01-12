c58492a08c27b650eb48bdd44a806725bc649ea0
Pavan Basa <pavanbasa@gmail.com>
2026-01-11 18:22:41 -0600

    Update stocks

--- stock.py
+++ stock.py
@@ -4,7 +4,9 @@ import requests
 import os
 
 
-symbols = ["AAPL", "MSFT", "GOOGL", "AMZN", "NVDA"]
+stocks_env = os.environ.get("STOCKS", "")
+symbols = [s.strip().upper() for s in stocks_env.split(",") if s.strip()]
+
 lines = []
 
 ### telegram
@@ -16,12 +18,18 @@ for stock in symbols:
     ticker = yf.Ticker(stock)
 
     # Get latest price
-    current_price = ticker.info["currentPrice"]
-    previous_close = ticker.info["previousClose"]
+    #current_price = ticker.info["currentPrice"]
+    #previous_close = ticker.info["previousClose"]
+    
+
+    
+    current_price = ticker.fast_info["last_price"]
+    previous_close = ticker.fast_info["previous_close"]
+
     trend = "↑" if current_price > previous_close else "↓"
     stock_name = ticker.info["shortName"]
     
-    lines.append(f"{stock_name:<25} : {trend}{current_price}")
+    lines.append(f"{stock_name:<25} : {trend}{current_price:.2f}")
     
 #message = "📊 Stock Update\n```\n" + "\n".join(lines) + "\n```"
 message = "\n".join(lines)
